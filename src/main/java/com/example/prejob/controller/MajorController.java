81c3f55cbd8873888091e98c6ba65f2e54f1dc01
krajniy <krajniy@gmail.com>
2023-03-22 16:50:14 +0100

    backend v1.2, CORS enabled

--- src/main/java/com/example/prejob/controller/MajorController.java
+++ src/main/java/com/example/prejob/controller/MajorController.java
@@ -5,6 +5,7 @@ import com.example.prejob.service.MajorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class MajorController {
     @Autowired
     MajorService majorService;
 
+    @CrossOrigin(origins = "*")
     @GetMapping()
     public ResponseEntity<List<Major>> getAllMajors(){
         try {
